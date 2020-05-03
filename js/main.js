8feb1b2e6b1101db6ebe69efed0dcfc8610e27c8
Collin O'Connor <collin.d.oconnor@gmail.com>
2020-05-03 17:37:03 +0000

    Moving some of the buttons, and adding in support for checking or unchecking all boxes.

--- js/main.js
+++ js/main.js
@@ -65,3 +65,14 @@ function printLists() {
         makeList(key, value);
     }
 }
+
+function checkAll(checktoggle) {
+  var checkboxes = new Array();
+  checkboxes = document.getElementsByTagName('input');
+
+  for (var i=0; i<checkboxes.length; i++)  {
+    if (checkboxes[i].type == 'checkbox')   {
+      checkboxes[i].checked = checktoggle;
+    }
+  }
+}
